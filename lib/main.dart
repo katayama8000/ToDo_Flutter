1572a14427013aab4cce9ac6aad93bf187d78030
katayama <tattu.0310@gmail.com>
2022-06-15 11:56:15 +0900

    配列を表示

--- lib/main.dart
+++ lib/main.dart
@@ -5,103 +5,54 @@ import 'package:flutter/material.dart';
 
 import 'addnote.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
-}
+void main() => runApp(const MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "todo app",
-      theme: ThemeData(
-        primaryColor: Colors.greenAccent[700],
-      ),
-      home: Home(),
-      debugShowCheckedModeBanner: false,
+    return const MaterialApp(
+      home: SimpleToDoApp(),
     );
   }
 }
 
-class Home extends StatefulWidget {
+class SimpleToDoApp extends StatefulWidget {
+  const SimpleToDoApp({Key? key}) : super(key: key);
+
   @override
-  _HomeState createState() => _HomeState();
+  State<SimpleToDoApp> createState() => _SimpleToDoAppState();
 }
 
-class _HomeState extends State<Home> {
-  final fb = FirebaseDatabase.instance;
+class _SimpleToDoAppState extends State<SimpleToDoApp> {
+  List<String> todos = [
+    "買い物",
+    "掃除",
+    "洗濯",
+  ];
   @override
   Widget build(BuildContext context) {
-    final ref = fb.ref().child('todos');
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.indigo[900],
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (_) => addnote(),
-            ),
-          );
-        },
-        child: Icon(
-          Icons.add,
-        ),
-      ),
       appBar: AppBar(
-        title: Text(
-          'Todos',
-          style: TextStyle(
-            fontSize: 30,
-          ),
-        ),
-        backgroundColor: Colors.indigo[900],
+        title: const Text('ToDo_PINK'),
+        backgroundColor: const Color.fromARGB(255, 251, 70, 130),
       ),
-      body: FirebaseAnimatedList(
-        query: ref,
-        //shrinkWrap: true,
-        itemBuilder: (context, snapshot, animation, index) {
-          return GestureDetector(
-            onTap: () {},
-            child: Container(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ListTile(
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(
-                      color: Colors.white,
-                    ),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  tileColor: Colors.indigo[100],
-                  trailing: IconButton(
-                    icon: Icon(
-                      Icons.delete,
-                      color: Colors.red[900],
-                    ),
-                    onPressed: () {
-                      ref.child(snapshot.key!).remove();
-                    },
-                  ),
-                  title: Text(
-                    snapshot.value.toString(),
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
+      body: Center(
+        child: ListView.builder(
+          itemCount: todos.length,
+          itemBuilder: (context, index) {
+            final item = todos[index];
+
+            return Card(
+                shape: RoundedRectangleBorder(
+                  side: const BorderSide(
+                      color: Color.fromARGB(255, 251, 70, 130)),
+                  borderRadius: BorderRadius.circular(20),
                 ),
-              ),
-            ),
-          );
-        },
+                child: ListTile(title: Text(item)));
+          },
+        ),
       ),
     );
   }
